db98dfe6aea54fcbb153e76ff147add3230e577d
SuriPro <surya111cs@gmail.com>
2019-12-17 18:11:16 +0530

    just updated

--- app/src/main/java/com/example/toast/ToasterMessage.java
+++ app/src/main/java/com/example/toast/ToasterMessage.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
-        public static void toast(Context c, String message){
 
-            Toast.makeText(c,message,Toast.LENGTH_LONG).show();
+    public static void toastShort(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
 
-        }
+    public static void toastLong(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_LONG).show();
+    }
+
 }
